c73c37eaea71dfc9442d17a968314339c85083c9
George Fleming <67323735+Grogfl@users.noreply.github.com>
2024-08-11 17:51:44 +0100

    player and landing craft hp

---
+++ Final Project/Assets/Scripts/landingCraft.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class landingCraft : MonoBehaviour
+{
+    public int maxHealth = 100;
+    private int currentHealth;
+    //public Text healthText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = maxHealth;
+        //UpdateHealthText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Die();
+        }
+        //UpdateHealthText();
+    }
+
+    // void UpdateHealthText()
+    // {
+    //     if (healthText != null)
+    //     {
+    //         healthText.text = "Landing Craft Health: " + currentHealth;
+    //     }
+    // }
+
+    void Die()
+    {
+        // Implement what happens when the landing craft is destroyed
+        Debug.Log("Landing Craft destroyed!");
+        // You can add more logic here like ending the game, showing a game over screen, etc.
+    }
+}
